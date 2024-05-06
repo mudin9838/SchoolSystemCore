447c6d87cb66adf2358713abf40439b2715da560
Muhdin <muhdinmussema@gmail.com>
2024-05-06 08:40:39 +0300

    xgxg

--- Program.cs
+++ Program.cs
@@ -11,14 +11,14 @@ builder.Services.AddSqlServer<CollegeDbContext>(sqlConnection,
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
+app.UseExceptionHandler("/Home/Error");
+app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
